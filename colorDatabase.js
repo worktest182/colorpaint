328cdf3563521fc09b4363c29d774e19d72cb052
worktest182 <worktest182@gmail.com>
2026-02-24 18:42:51 +0400

    colorDatabase.js
    Тестим скрипт

---
+++ colorDatabase.js
@@ -0,0 +1,15 @@
+// testColorDatabase.js
+window.colorDatabase = {
+  dulux: {
+    "50GG 83/034": "#B0C97F"  // HEX для внутреннего применения
+  },
+  iceparade: {
+    "AP5-2": "#C8DFFF"
+  },
+  vision: {
+    "VN2354": "#F2C6B3"
+  },
+  ral: {
+    "9023": "#F2F2F2"
+  }
+};
